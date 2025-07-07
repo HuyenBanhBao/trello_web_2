f90b9c3b2357f03d44a17447b11aae648ea04dfb
HuyenBanhBao <ngoctung2307th@gmail.com>
2025-07-07 22:53:31 +0700

    Update board invitation

--- src/pages/Boards/BoardBar/InviteBoardUser.jsx
+++ src/pages/Boards/BoardBar/InviteBoardUser.jsx
@@ -10,6 +10,7 @@ import { useForm } from "react-hook-form";
 import { EMAIL_RULE, FIELD_REQUIRED_MESSAGE, EMAIL_RULE_MESSAGE } from "~/utils/validators";
 import FieldErrorAlert from "~/components/Form/FieldErrorAlert";
 import { inviteUserToBoardAPI } from "~/apis";
+import { socketIoInstance } from "~/socketClient";
 
 function InviteBoardUser({ board }) {
     /**
@@ -35,12 +36,13 @@ function InviteBoardUser({ board }) {
 
         // console.log("inviteeEmail:", inviteeEmail);
         // Gọi API mời user vào làm thành viên của Board
-        inviteUserToBoardAPI({ inviteeEmail, boardId: board._id }).then(() => {
+        inviteUserToBoardAPI({ inviteeEmail, boardId: board._id }).then((invitation) => {
             // Clear thẻ input sử dụng react-hook-form bằng setValue, Đồng thời đóng modal lại
             setValue("inviteeEmail", null);
             setAnchorPopoverElement(null);
 
             // Mời một người dùng vào board xong thì cũng sẽ gửi/emit sự kiện socket lên server (tính năng real-time)
+            socketIoInstance.emit("FE_USER_INVITED_TO_BOARD", invitation);
         });
     };
 
